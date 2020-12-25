d48fa9a5fc55a5c53918035b111622a75cfca6a8
= <=>
2020-12-25 11:53:30 +0700

    2 Практика законченная работа

--- Практика 1/Практика1.js
+++ Практика 1/Практика1.js
@@ -7,10 +7,46 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-let movielastone1 = prompt("Один из последних просмотренных фильмов", "");
-let scoremovie1 = prompt("Насколько вы его оцените", "");
-let movielastone2 = prompt("Второй из последних просмотренных фильмов", "");
-let scoremovie2 = prompt("Насколько вы его оцените", "");
-personalMovieDB.movies[movielastone1] = scoremovie1;
-personalMovieDB.movies[movielastone2] = scoremovie2;
+for (numberOfFilms; numberOfFilms > 0; numberOfFilms--) {
+    let movielastone = prompt("Как называется этот фильм", "");
+    let scoremovie = +prompt("Насколько вы его оцените", "");
+    if (
+        movielastone != null &&
+        scoremovie != null &&
+        movielastone != "" &&
+        scoremovie != "" &&
+        movielastone.length < 50
+    ) {
+        personalMovieDB.movies[movielastone] = scoremovie;
+        console.log("You win");
+    } else {
+        console.log("You broked");
+        numberOfFilms++;
+    }
+}
+if (personalMovieDB.count <= 10 && personalMovieDB.count >= 0) {
+    console.log(" Вы посмотрели слишком мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log(" Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log(" Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 console.log(personalMovieDB);
+// Это еще два вариана цикла:
+// if (personalMovieDB.count < 10) ? console.log(" Вы посмотрели слишком мало фильмов") : console.log("");
+// if (personalMovieDB.count > 10 && personalMovieDB.count < 30) ?: console.log(" Вы классический зритель");
+// if (personalMovieDB.count > 30) ?: console.log(" Вы киноман");
+
+// switch (personalMovieDB.count) {
+//     case 0:
+//         console.log(" Вы посмотрели слишком мало фильмов");
+//         break;
+//     case 10 - 30:
+//         console.log(" Вы классический зритель");
+//         break;
+//     case (30 - 2) ^ 50:
+//         console.log(" Вы киноман");
+//         break;
+// }
---
+++ Просто код/Просто код.js
@@ -0,0 +1,6 @@
+"use strict";
+let nem = 50; 
+while(nem <= 55){
+    console.log(nem);
+    nem++;
+}
