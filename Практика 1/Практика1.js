80719d18d508b60da31534759b60c1c0ae44e425
Ilya <ilya.battalov@bk.ru>
2020-12-25 15:00:11 +0700

    3 законченная практика

--- Практика 1/Практика1.js
+++ Практика 1/Практика1.js
@@ -1,5 +1,13 @@
 "use srtict";
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ", "");
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ", "");
+    }
+}
+start();
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,33 +15,56 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-for (numberOfFilms; numberOfFilms > 0; numberOfFilms--) {
-    let movielastone = prompt("Как называется этот фильм", "");
-    let scoremovie = +prompt("Насколько вы его оцените", "");
-    if (
-        movielastone != null &&
-        scoremovie != null &&
-        movielastone != "" &&
-        scoremovie != "" &&
-        movielastone.length < 50
-    ) {
-        personalMovieDB.movies[movielastone] = scoremovie;
-        console.log("You win");
-    } else {
-        console.log("You broked");
-        numberOfFilms++;
+
+function rememberMyFilms() {
+    for (numberOfFilms; numberOfFilms > 0; numberOfFilms--) {
+        let movielastone = prompt("Как называется этот фильм", "");
+        let scoremovie = +prompt("Насколько вы его оцените", "");
+        if (
+            movielastone != null &&
+            scoremovie != null &&
+            movielastone != "" &&
+            scoremovie != "" &&
+            movielastone.length < 50
+        ) {
+            personalMovieDB.movies[movielastone] = scoremovie;
+            console.log("You win");
+        } else {
+            console.log("You broked");
+            numberOfFilms++;
+        }
     }
 }
-if (personalMovieDB.count <= 10 && personalMovieDB.count >= 0) {
-    console.log(" Вы посмотрели слишком мало фильмов");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    console.log(" Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log(" Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count <= 10 && personalMovieDB.count >= 0) {
+        console.log(" Вы посмотрели слишком мало фильмов");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log(" Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log(" Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB(hidden) { 
+    if (hidden == false) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
+
+
 // Это еще два вариана цикла:
 // if (personalMovieDB.count < 10) ? console.log(" Вы посмотрели слишком мало фильмов") : console.log("");
 // if (personalMovieDB.count > 10 && personalMovieDB.count < 30) ?: console.log(" Вы классический зритель");
--- Просто код/Просто код.js
+++ Просто код/Просто код.js
@@ -1,6 +1,16 @@
 "use strict";
-let nem = 50; 
-while(nem <= 55){
-    console.log(nem);
-    nem++;
+let num = 20;
+function showMessage(text){
+console.log(text);
+num =30;
 }
+showMessage('Привет');
+console.log(num);
+
+const summ = (a, b) => a+b;
+
+const minus = function(a,b){
+return a-b;
+};
+console.log(summ(300, 100));
+console.log(minus(300, 100));
