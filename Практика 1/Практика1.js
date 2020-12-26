7e2450e70603dcace5fe840772505cac92f46057
= <=>
2020-12-26 20:11:37 +0700

    Готовая 4 практическая работа

--- Практика 1/Практика1.js
+++ Практика 1/Практика1.js
@@ -1,83 +1,79 @@
 "use srtict";
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ", "");
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ", "");
-    }
-}
-start();
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-};
-
-function rememberMyFilms() {
-    for (numberOfFilms; numberOfFilms > 0; numberOfFilms--) {
-        let movielastone = prompt("Как называется этот фильм", "");
-        let scoremovie = +prompt("Насколько вы его оцените", "");
-        if (
-            movielastone != null &&
-            scoremovie != null &&
-            movielastone != "" &&
-            scoremovie != "" &&
-            movielastone.length < 50
+    start: function () {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели ", "").toLowerCase();
+        while (
+            personalMovieDB.count == "" ||
+            personalMovieDB.count == null ||
+            isNaN(personalMovieDB.count)
         ) {
-            personalMovieDB.movies[movielastone] = scoremovie;
-            console.log("You win");
-        } else {
-            console.log("You broked");
-            numberOfFilms++;
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели ", "").toLowerCase();
         }
+    },
+    rememberMyFilms: function () {
+        for (personalMovieDB.count; personalMovieDB.count > 0; personalMovieDB.count--) {
+            let movielastone = prompt("Как называется этот фильм", "").toLowerCase();
+            let scoremovie = +prompt("Насколько вы его оцените", "").toLowerCase();
+            if (
+                movielastone != null &&
+                scoremovie != null &&
+                movielastone != "" &&
+                scoremovie != "" &&
+                movielastone.length < 50
+            ) {
+                personalMovieDB.movies[movielastone] = scoremovie;
+                console.log("You win");
+            } else {
+                console.log("You broked");
+                personalMovieDB.count++;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count <= 10 && personalMovieDB.count >= 0) {
+            console.log(" Вы посмотрели слишком мало фильмов");
+        } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+            console.log(" Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log(" Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function (hidden) {
+        if (hidden == false) {
+            console.log(personalMovieDB);
+        }
+    },
+    toogleVisibleMyDB: function () {
+        if (personalMovieDB.privat == false) {
+            personalMovieDB.privat = true;
+        } else {
+            personalMovieDB.privat = false;
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i <= 3; i++) {
+            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
+            while (personalMovieDB.genres[i - 1] == "" ||
+                personalMovieDB.genres[i - 1] == null ||
+                personalMovieDB.genres[i - 1] == isNaN(personalMovieDB.genres)) {
+                personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
-}
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count <= 10 && personalMovieDB.count >= 0) {
-        console.log(" Вы посмотрели слишком мало фильмов");
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        console.log(" Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log(" Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-detectPersonalLevel();
-
-function showMyDB(hidden) { 
-    if (hidden == false) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
-
-
-// Это еще два вариана цикла:
-// if (personalMovieDB.count < 10) ? console.log(" Вы посмотрели слишком мало фильмов") : console.log("");
-// if (personalMovieDB.count > 10 && personalMovieDB.count < 30) ?: console.log(" Вы классический зритель");
-// if (personalMovieDB.count > 30) ?: console.log(" Вы киноман");
-
-// switch (personalMovieDB.count) {
-//     case 0:
-//         console.log(" Вы посмотрели слишком мало фильмов");
-//         break;
-//     case 10 - 30:
-//         console.log(" Вы классический зритель");
-//         break;
-//     case (30 - 2) ^ 50:
-//         console.log(" Вы киноман");
-//         break;
-// }
+};
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.writeYourGenres();
+personalMovieDB.toogleVisibleMyDB();
--- Просто код/Просто код.js
+++ Просто код/Просто код.js
@@ -1,16 +1,74 @@
 "use strict";
-let num = 20;
-function showMessage(text){
-console.log(text);
-num =30;
-}
-showMessage('Привет');
-console.log(num);
+// const options = {
+//     name: 'Test',
+//     width: 1024,
+//     height: 512,
+//     colors: {
+//         border: "black",
+//         bg: "red"
+//     }
+// };
+// const {border, bg} = options.colors;
+// console.log(`border = ${border} .BG = ${bg}`);
 
-const summ = (a, b) => a+b;
 
-const minus = function(a,b){
-return a-b;
+// for (let key in options) {
+//     if (typeof (options[key]) === "object") {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+// function leanJS(lang, callback){
+//     console.log(` Я учу ${lang} язык `);
+//     callback();
+// }
+//  const adv = function(){
+//     console.log(" Этот язык вообще изи");
+//     };
+// leanJS ("English", adv);
+// let numbers = {
+// //     a: 10,
+// //     b: 20,
+// //     c: {
+// //         x: 100,
+// //         y: 200,
+// //     }
+// // };
+// // let at = {
+// //     d: 17,
+// //     k: 20,
+// // };
+//   function copy(makecopy){
+//       let key;
+//       let newCopyObject = {};
+//       for (key in makecopy){
+//         newCopyObject[key]= makecopy[key];
+//       }
+//       return newCopyObject;
+//   }
+// let copyNumbers = Object.assign({}, numbers, at);
+// copyNumbers.d = 10000;
+// console.log(numbers);
+// console.log(copyNumbers);
+// const oldArr = ["a", "b", "c"];
+// const newArr = oldArr.slice();
+// newArr[0] = 'z';
+// console.log(oldArr);
+// console.log(newArr);
+// const video = ['youtube', 'vimeo', 'tik-tok'];
+// const blogs = ['VK', 'Twitter', 'Instagram'];
+// const internet = [...video,...blogs,'Github'];
+// console.log(internet);
+const erty = {
+    one: 1,
+    two: 2,
+    three: 3,
+    sayHello: function() {
+        console.log ("Привет");
+    }
 };
-console.log(summ(300, 100));
-console.log(minus(300, 100));
+const newErty = Object.create(erty);
+newErty.sayHello();
